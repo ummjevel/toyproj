7fd1b9d0b083451b29c55bb56bfa3bb5d9ffda2e
ummjeveloper <mseagle2023@gmail.com>
2020-04-25 16:20:12 +0900

    process 하다가 말더라도 /show를 하면 이어지도록 구현

--- kairos_alarm/kairos_test.py
+++ kairos_alarm/kairos_test.py
@@ -26,9 +26,9 @@ list_message_ko = [
     ,"미리알림을 받을 시간을 선택해주세요." # 15
     ,"지금 투표하고 싶으실 경우 아래 버튼을 눌러주세요." # 16
     ,"{} 시로 저장되었고, {} 명의 투표가 남았습니다." # 17
-    ,"현재 상태: \n" # 18
+    ,"투표 현황: \n" # 18
     ,"    {}시: {}명\n" # 19
-    ,""
+    ,"투표 참여/총 인원: {}/{}\n" # 20
 ]
 list_button_ko = [
     ["예", "yes"]
@@ -67,7 +67,7 @@ list_command = [
 
 dict_data = {}
 
-CREATE, REPEAT, CHOOSE, CHOOSE_CHK, ASK, VOTE, ENSURE, FIX, PREACQ, COMP, FIX_TDAY, PREACQ2, ASK_CHK = range(13)
+CREATE, REPEAT, CHOOSE, CHOOSE_CHK, ASK, VOTE, ENSURE, FIX, PREACQ, COMP, FIX_TDAY, PREACQ2, ASK_CHK, KEEP_CHOOSE = range(14)
 
 # 언어 설정
 list_button = list_button_ko
@@ -76,11 +76,17 @@ list_message = list_message_ko
 # { 채널 id: 현재상태... } list_channel.update(a=1)
 list_channel = {}
 
-def makebutton(index, text = None):
+def makebutton(index, text = None, value = ""):
     if text is None:
-        return [InlineKeyboardButton(list_button[index][0], callback_data=list_button[index][1])]
+        if value == "":
+            return [InlineKeyboardButton(list_button[index][0], callback_data=list_button[index][1])]
+        else:
+            return [InlineKeyboardButton(list_button[index][0], callback_data=value)]
     else:
-        return [InlineKeyboardButton(text, callback_data=list_button[index][1])]
+        if value == "":
+            return [InlineKeyboardButton(text, callback_data=list_button[index][1])]
+        else:
+            return [InlineKeyboardButton(text, callback_data=value)]
 
 kairos_token = "1110549427:AAHYDs1Lo3zUmSUpprI_qN04m-ekRSfvhxw"
 updater = Updater(kairos_token) #, use_context=True)
@@ -120,19 +126,19 @@ def repeat(update, context):
         )
         del dict_data[chat_id]
         return ConversationHandler.END
-
-    update.edit_message_text(
-        chat_id=chat_id
-        ,message_id=query.message.message_id
-        ,text=list_message[2] # 반복할거야?
-    )
-    reply_markup = InlineKeyboardMarkup([makebutton(0), makebutton(1)]) # 예 아니오 이전
-    update.edit_message_reply_markup(
-        chat_id=chat_id
-        ,message_id=query.message.message_id
-        ,reply_markup=reply_markup
-    )
-    return CHOOSE
+    elif query.data == "yes":
+        update.edit_message_text(
+            chat_id=chat_id
+            ,message_id=query.message.message_id
+            ,text=list_message[2] # 반복할거야?
+        )
+        reply_markup = InlineKeyboardMarkup([makebutton(0), makebutton(1)]) # 예 아니오 이전
+        update.edit_message_reply_markup(
+            chat_id=chat_id
+            ,message_id=query.message.message_id
+            ,reply_markup=reply_markup
+        )
+        return CHOOSE
 
 def choose(update, context):
     query = context.callback_query
@@ -147,6 +153,9 @@ def choose(update, context):
     elif query.data == "no":
         choose_msg = list_message[11]
         dict_data[chat_id]['repeat'] = False
+    else:
+        # 중간에 다시 설정하려고 할 때..
+        pass
     # 실수는 용납하지 않는다.
     # else:
     #     reply_markup = InlineKeyboardMarkup([makebutton(0), makebutton(1)])
@@ -193,6 +202,7 @@ def choose_chk(update, context):
 
         # save week
         # 
+        dict_data[chat_id]['setweek'] = 'done'
         update.edit_message_text(
             chat_id=chat_id
             ,message_id=query.message.message_id
@@ -205,8 +215,23 @@ def choose_chk(update, context):
             ,reply_markup=reply_markup
         )
         return ASK
+    elif query.data == "keep_yes":
+        dict_data[chat_id]['week'] = [False, False, False, False, False, False, False]
+        reply_markup = InlineKeyboardMarkup([makebutton(6), makebutton(7),makebutton(8), makebutton(9),makebutton(10), makebutton(11),makebutton(12),makebutton(13)]) #, makebutton(2)]) 
+    
+        update.edit_message_text(text=list_message[13]
+                                , chat_id=chat_id
+                                , message_id=query.message.message_id
+                                , reply_markup=reply_markup)
+    elif query.data == "keep_no":
+        #reply_markup = InlineKeyboardMarkup([makebutton(6), makebutton(7),makebutton(8), makebutton(9),makebutton(10), makebutton(11),makebutton(12),makebutton(13)]) #, makebutton(2)]) 
+    
+        update.edit_message_text(text="알겠습니다."
+                                , chat_id=chat_id
+                                , message_id=query.message.message_id)
+                                #, reply_markup=reply_markup)
+        #return ConversationHandler.END
     else:
-        
         list_button_week = []
         week_index = list_week.index(query.data)
         if dict_data[chat_id]['week'][week_index] == False:
@@ -274,6 +299,12 @@ def ask(update, context):
         dict_data[chat_id]['setwhen'] = 'tday'
         #return ENSURE
         return ASK_CHK
+    elif query.data == "end":
+        update.edit_message_text(text="알겠습니다."
+                                , chat_id=chat_id
+                                , message_id=query.message.message_id)
+                                #, reply_markup=reply_markup)
+        #return ConversationHandler.END
     # elif query.data == "prev":
     #     return ASK
 
@@ -391,6 +422,12 @@ def preacq2(update, context):
         )
         dict_data[chat_id]['preacq'] = False
         return ConversationHandler.END
+    elif query.data == "end":
+        update.edit_message_text(text="알겠습니다."
+                                , chat_id=chat_id
+                                , message_id=query.message.message_id)
+        #return ConversationHandler.END
+
 
 def comp(update, context):
     query = context.callback_query
@@ -398,22 +435,34 @@ def comp(update, context):
     #list_channel[chat_id] = 'comp'
     # save state
     dict_data[chat_id]['state'] = 'comp'
-    update.edit_message_text(
-        chat_id=chat_id
-        ,message_id=query.message.message_id
-        ,text=list_message[9] # 설정완료
-    )
+    
     if query.data == 'cancel':
         dict_data[chat_id]['preacq'] = False
+        update.edit_message_text(
+            chat_id=chat_id
+            ,message_id=query.message.message_id
+            ,text=list_message[9] # 설정완료
+        ) 
+        return ConversationHandler.END  
+    elif query.data == 'end':
+        update.edit_message_text(text="알겠습니다."
+                                , chat_id=chat_id
+                                , message_id=query.message.message_id)
     else:
         dict_data[chat_id]['preacqtime'] = query.data
+        update.edit_message_text(
+            chat_id=chat_id
+            ,message_id=query.message.message_id
+            ,text=list_message[9] # 설정완료
+        )   
+        return ConversationHandler.END
     # reply_markup = InlineKeyboardMarkup([makebutton(0), makebutton(1), makebutton(2)])
     # update.edit_message_reply_markup(
     #     chat_id=query.message.chat_id
     #     ,message_id=query.message.message_id
     #     ,reply_markup=reply_markup
     # )
-    return ConversationHandler.END
+    
 
 def fixtime_tday(update, context, args):
     chat_id = context.message.chat_id
@@ -421,14 +470,18 @@ def fixtime_tday(update, context, args):
     # save state
     dict_data[chat_id]['state'] = 'fixtime_tday'
     #if args[0] >= 0 and args[0] <= 23 then save, or show message and go fix_chk
-    input_time = int(args[0])
-    if 0 <= input_time and input_time <= 23:
-        # save
-        reply_markup = InlineKeyboardMarkup([makebutton(0), makebutton(1)]) #, makebutton(2)])
-        context.message.reply_text(list_message[10].format(args[0]) + list_message[8], reply_markup=reply_markup) 
-         # 시간 설정 완료했으니 당일 알림 줄게 # 미리알림 하쉴?
-        dict_data[chat_id]['tdayalarmtime'] = input_time
-        return PREACQ2
+    if args[0].isdigit():
+        input_time = int(args[0])
+        if 0 <= input_time and input_time <= 23:
+            # save
+            reply_markup = InlineKeyboardMarkup([makebutton(0), makebutton(1)]) #, makebutton(2)])
+            context.message.reply_text(list_message[10].format(args[0]) + list_message[8], reply_markup=reply_markup) 
+            # 시간 설정 완료했으니 당일 알림 줄게 # 미리알림 하쉴?
+            dict_data[chat_id]['tdayalarmtime'] = input_time
+            return PREACQ2
+        else:
+            context.message.reply_text(list_message[14].format('t')) # 제대로 입력하라고 했잖니 0-23
+            return ConversationHandler.END
     else:
         context.message.reply_text(list_message[14].format('t')) # 제대로 입력하라고 했잖니 0-23
         return ConversationHandler.END
@@ -457,7 +510,6 @@ def votetime(update, context, args):
     #list_channel[chat_id] = 'votetime'
     # save state
     # dict_data[chat_id]['state'] = 'votetime'
-    dict_data[chat_id] = {}
     dict_data[chat_id]['voted'] = {}
     input_time = int(args[0])
     sender_id = context.message.from_user.id
@@ -488,6 +540,8 @@ def votetime(update, context, args):
         return ConversationHandler.END
 
 
+
+
 def show(update, context):
     # 리스트에 채널명으로 검사해서 있으면 보여준다
     chat_id = context.message.chat_id
@@ -497,58 +551,107 @@ def show(update, context):
         # 상태 값에 따라서 보여주기..
         this_data = dict_data[chat_id]
         strtext = ""
-        if this_data['setdone'] in this_data:
-            if this_data['setdone'] == True:
-                strtext += "알람은 {}시로 설정되었습니다.\n".format(this_data['alaramtime'])
-                # 요일 formatting.
-                this_week = ""
-                strtext += "요일은 {} 입니다.".format(this_week)
-                if this_data['preacq'] == True:
-                    strtext += "미리알림은 {}시 입니다.\n".format(this_data['preacqtime'])
-                else:
-                    strtext += "미리알림은 설정되어있지 않습니다.\n."
+        
+        # 반복
+        if 'repeat' in this_data:
+            if this_data['repeat'] == True:
+                strtext += "알림은 주마다 반복됩니다.\n"
             else:
-                #state 값에 따라서 다르게 보여주기.
-                if this_data['setwhen'] in this_data:
+                strtext += "알림은 주마다 반복되지 않는 일회성 알림입니다.\n"
+            # 요일 설정 표시
+            if 'setweek' in this_data:
+                # 요일 formatting.
+                this_week = [list_button[index + 6][0] for index, val in enumerate(this_data['week']) if val == True ] 
+                strtext += "알림 요일은 {} 입니다.\n".format(','.join(this_week))
+
+                if 'setwhen' in this_data:
                     if this_data['setwhen'] == 'now':
                         # 현재 투표 상태 표시
-                        pass
+                        # 명 이 {} 시에 투표.
+                        
+                        # 투표 다 끝냈을때
+                        #if 'setdone' in this_data:
+                        if this_data['setdone'] == True:
+                            strtext += "알람은 {}시로 설정되었습니다.\n".format(this_data['alarmtime'])
+                            
+                            if 'preacq' in this_data:
+                                if this_data['preacq'] == True:
+                                    if 'preacqtime' in this_data:
+                                        preacq_value = [i[0] for i in list_button if i[1] == this_data['preacqtime']]
+                                        strtext += "미리알림은 {} 입니다.\n".format(preacq_value[0])
+                                        context.message.reply_text(strtext)
+                                    else:
+                                        strtext += "미리알림은 사용하시되 시간을 안정하셨군요.\n"
+                                        
+                                        # 3h 1h 30min 10min cancel
+                                        reply_markup = InlineKeyboardMarkup([makebutton(14), makebutton(15),makebutton(16), makebutton(17), makebutton(18), makebutton(0, "지금은 안 할래요", "end")]) 
+                                        
+                                        context.message.reply_text(strtext + list_message[15], reply_markup=reply_markup)
+                                else:
+                                    strtext += "미리알림은 없습니다.\n"
+                                    context.message.reply_text(strtext)
+                            else:
+                                strtext += "미리알림은 설정되어있지 않습니다.\n미리알림을 설정하시겠어요?\n"
+                                # 미리알림 설정할래? 예 아니오 지금 안할래요.
+                                reply_markup = InlineKeyboardMarkup([makebutton(0), makebutton(1), makebutton(0, "지금은 안 할래요", "end")]) #, makebutton(2)])
+                                context.message.reply_text(strtext, reply_markup=reply_markup)
+                        else:
+                            if this_data['voted']:
+                                strtext += list_message[18]
+                                voted_result = Counter([val for val in this_data['voted'].values()]).most_common()
+                                #determin_time = Counter([val for val in this_data['voted'].values()]).most_common(1)[0][0]
+                                for index in range(0, len(voted_result)):
+                                    strtext += list_message[19].format(voted_result[index][0], voted_result[index][1])
+                            
+                            # 참여인원
+                            strtext += list_message[20].format(this_data['votecnt'], context.message.bot.get_chat_members_count(chat_id) - 1)
+
+                            context.message.reply_text(strtext)
                     else:
                         # 당일 알림 시간 표시
-                        if this_data['tdayalarmtime'] in this_data:
+                        if 'tdayalarmtime' in this_data:
                             strtext += "당일 투표알림은 {}시 입니다.\n".format(this_data['tdayalarmtime'])
+                            if 'preacq' in this_data:
+                                if this_data['preacq'] == True:
+                                    if 'preacqtime' in this_data:
+                                        strtext += "미리알림은 {}시 입니다.\n".format(this_data['preacqtime'])
+                                        context.message.reply_text(strtext)
+                                    else:
+                                        strtext += "미리알림은 사용하시되 시간을 안정하셨군요.\n미리알림 시간을 설정하시겠어요?"
+                                        
+                                        # 3h 1h 30min 10min cancel
+                                        reply_markup = InlineKeyboardMarkup([makebutton(14), makebutton(15),makebutton(16), makebutton(17), makebutton(18), makebutton(0, "지금은 안 할래요", "end")]) 
+                                        
+                                        context.message.reply_text(strtext + list_message[15], reply_markup=reply_markup)
+                                else:
+                                    strtext += "미리알림은 사용하지 않습니다.\n"
+                                    context.message.reply_text(strtext)
+                            else:
+                                strtext += "미리알림은 설정되어있지 않습니다. \n미리알림을 설정하시겠어요?\n"
+                                # 미리알림 설정할래? 예 아니오 지금 안할래요.
+                                reply_markup = InlineKeyboardMarkup([makebutton(0), makebutton(1), makebutton(0, "지금은 안 할래요", "end")]) #, makebutton(2)])
+                                context.message.reply_text(strtext, reply_markup=reply_markup)
                         else:
-                            strtext += "당일 투표알림이 설정되었지만 시간은 설정되어있지 않네요. 지금 설정하시겠어요?\n"
-                            reply_markup = InlineKeyboardMarkup([makebutton(0), makebutton(1)]) #, makebutton(2)])
-                            context.message.reply_text(strtext, reply_markup=reply_markup)
-                            return PREACQ2
+                            strtext += "당일 투표알림이 설정되었지만 시간은 설정되어있지 않네요.\n /t [0-23] 을 입력하여 설정해주세요.\n"
+                            reply_markup = InlineKeyboardMarkup([makebutton(4)]) # 지금부터
+                            context.message.reply_text(strtext + list_message[16], reply_markup=reply_markup)
+                            
                 else:
-                    # 반복
-                    if this_data['repeat'] in this_data:
-                        if this_data['repeat'] == True:
-                            strtext += "알림은 주마다 반복됩니다.\n"
-                        else:
-                            strtext += "알림은 주마다 반복되지 않는 일회성 알림입니다.\n"
-                        # 요일 설정 표시
-                        if this_data['setdone'] in this_data:
-                            # 요일 설정 완료까지 한 상태 인데 당일알림인지 지금부터인지 투표시간은 안 정한 상태 (정했으면 위 setwhen에 걸렸음)
-                            # 요일표시
-                            # 당일알림/지금투표인지 설정하는 부분으로 이동
-                            pass
-                        else:
-                            # 요일 설정까지는 안하고
-                            # week 에 하나도 체크 안 되어있으면 맨 처음 상태로 choose_chk인가? 
-                            # 체크 하나라도 true 되어 있으면 choose_chk인가로..
-                            pass
-                    else:
-                        # 생성만 한 상태
-                        strtext += "알람이 생성만 되어있네요. 설정을 이어가시겠어요? 이어가지 않을 경우 생성된 알람은 삭제됩니다.\n"
-                        reply_markup = InlineKeyboardMarkup([makebutton(0), makebutton(1)]) #, makebutton(2)])
-                        context.message.reply_text(strtext, reply_markup=reply_markup)
-                        return REPEAT
-
-
-
+                    # 요일 설정 완료까지 한 상태 인데 당일알림인지 지금부터인지 투표시간은 안 정한 상태 (정했으면 위 setwhen에 걸렸음)
+                    # 요일표시
+                    # 당일알림/지금투표인지 설정하는 부분으로 이동
+                    strtext += "투표시간을 정하는 중이시네요. 다시 이어가시겠어요?\n"
+                    reply_markup = InlineKeyboardMarkup([makebutton(4), makebutton(5), makebutton(0, "지금은 안 할래요", "end")]) #, makebutton(2)])
+                    context.message.reply_text(strtext, reply_markup=reply_markup)
+            else:
+                # 요일 설정까지 하다말았네
+                strtext += "요일설정 중이시네요. 요일설정부터 다시 이어가시겠어요?\n"
+                reply_markup = InlineKeyboardMarkup([makebutton(0, None, "keep_yes"), makebutton(1, None, "keep_no")]) #, makebutton(2)])
+                context.message.reply_text(strtext, reply_markup=reply_markup)
+            
+        else:
+            # 생성만 한 상태
+            strtext += "알람이 생성만 되어있네요. /create 명령어로 다시 처음부터 시작해주세요.\n"
             context.message.reply_text(strtext)
 
 
@@ -585,6 +688,7 @@ conv_handler = ConversationHandler(
         # ,FIX_TDAY: [CallbackQueryHandler(fixtime_tday)]
     }
     ,fallbacks=[CommandHandler(list_command[0], create)]
+    ,allow_reentry=True
 )
 
 updater.dispatcher.add_handler(conv_handler)
@@ -627,6 +731,26 @@ updater.dispatcher.add_handler(conv_handler4)
 # updater.dispatcher.add_handler(CommandHandler('create', create))
 
 updater.dispatcher.add_handler(CommandHandler('show', show))
+# conv_handler5 = ConversationHandler(
+#     entry_points=[CommandHandler('show', show)]
+#     ,states={
+#          CREATE: [CallbackQueryHandler(create)]
+#         ,REPEAT: [CallbackQueryHandler(repeat)]
+#         ,ASK: [CallbackQueryHandler(ask)]
+#         ,ASK_CHK: [CallbackQueryHandler(ask_chk)]
+#         ,VOTE: [CallbackQueryHandler(vote)]
+#         #,ENSURE: [CallbackQueryHandler(ensure)]
+#         #,FIX: [CallbackQueryHandler(fix)]
+#         ,PREACQ: [CallbackQueryHandler(preacq)]
+#         ,PREACQ2: [CallbackQueryHandler(preacq2)]
+#         ,COMP: [CallbackQueryHandler(comp)]
+#     }
+#     ,fallbacks=[CommandHandler('show', show)]
+#     ,allow_reentry=True
+# )
+# updater.dispatcher.add_handler(conv_handler5)
+
+
 updater.dispatcher.add_handler(CommandHandler('del', delete))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 
